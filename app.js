bbdc8593c8de5b4f3dd3bd373a29c67a82e1ea92
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-22 12:16:06 -0500

    Variables y constantes

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-console.log(2+2);
-console.log("Esto es un simulacro");
-console.log(true);
-console.log('ID: '+20);
+// Para comentarios de linea
+/*
+  Para comentarios de blques de codigo
+ */
+// var let y const
+
+// var
+var nombre = "Juan";
+nombre = 'John';
+var carrito;
+var var1 ='variable 1', var2 ='variable 2', var3 ='variable 3';
+console.log(nombre);
+console.table(carrito);
+
+// let
+// No se pueden redeclarar variables
+let  nombre2 = 'Juan';
+let producto = "Libro";
+let nombre3;
+let producto1='producto 1', producto2='producto 2', producto3='producto 3';
+console.log(nombre2);
+console.log(producto);
+console.log(nombre3);
+
+// const
+// No se pueden crear vacias deben estar inicializadas siempre
+const constante = 'Libro_constante';
+console.log(constante);
