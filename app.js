cf61fed12ca7ed95a103223e75524560e616ac11
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-22 00:17:08 -0500

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-document.getElementById('app').innerHTML = 'Hola Mundo';
+let nombre = prompt("Cual es tu nombre");
+let edad = prompt("Cual es tu edad");
+document.getElementById("app").innerHTML = `Bienvido ${nombre} de ${edad} años`;
