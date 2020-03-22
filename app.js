b1c66d958085106afe74ef37541179477a6da825
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-22 12:37:03 -0500

    Introduccion Strings

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-// Para comentarios de linea
-/*
-  Para comentarios de blques de codigo
- */
-// var let y const
-
-// var
-var nombre = "Juan";
-nombre = 'John';
-var carrito;
-var var1 ='variable 1', var2 ='variable 2', var3 ='variable 3';
+let nombre, mensaje;
+nombre ="Juan Pablo";
 console.log(nombre);
-console.table(carrito);
-
-// let
-// No se pueden redeclarar variables
-let  nombre2 = 'Juan';
-let producto = "Libro";
-let nombre3;
-let producto1='producto 1', producto2='producto 2', producto3='producto 3';
-console.log(nombre2);
-console.log(producto);
-console.log(nombre3);
-
-// const
-// No se pueden crear vacias deben estar inicializadas siempre
-const constante = 'Libro_constante';
-console.log(constante);
+// Si el texto tiene comillas dobles utiliza las simples y visceversa
+mensaje = 'Y entonces dije "Buen Curso"!!';
+// o escapando caracteres
+mensaje = 'Y entonces dije \'Buen Curso\'!!';
+console.log(mensaje)
+// COncattenar JAvaScript
+mensaje = "Java"+"Script";
+console.log(mensaje);
