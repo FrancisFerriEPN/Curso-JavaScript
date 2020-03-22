8e7eda04c3f9acea6be5df3c247ea7556f2163af
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-22 12:57:09 -0500

    Metodos Strings

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
-let nombre, mensaje;
-nombre ="Juan Pablo";
-console.log(nombre);
-// Si el texto tiene comillas dobles utiliza las simples y visceversa
-mensaje = 'Y entonces dije "Buen Curso"!!';
-// o escapando caracteres
-mensaje = 'Y entonces dije \'Buen Curso\'!!';
-console.log(mensaje)
-// COncattenar JAvaScript
-mensaje = "Java"+"Script";
-console.log(mensaje);
+let aprendiendo = "Arendiendo: ", tecnologia = 'JavaScript';
+console.log(aprendiendo+ "- "+tecnologia);
+// o
+// se puede espaciar directo e esta
+console.log(`${aprendiendo} ${tecnologia}`);
+// atributos son con puntos
+console.log(tecnologia.length);
+// No guarda la concatenacion
+tecnologia.concat(" Es genial");
+console.log(tecnologia);
+// Concatena directamente
+console.log(tecnologia.concat(" ","Es genial", " y bien chevere"));
+// UpperCase
+console.log(tecnologia.toUpperCase());
+let gato = "       gato vaca 1              ";
+console.log(gato.trim());
+// No te olvides que esto sirve para let 
+let mensaje = "Aprendiendo JavaScript, CSS, HTML, para ser Frontend";
+// Devuelve el indie en el cual comienza
+console.log(mensaje.indexOf("JavaScript"));
+// devuelve -1 si no enuentra el valor (exactamentte -1 no false o null)
+
+console.log(mensaje.indexOf("Php"));
+// Se especifica limites
+console.log(mensaje.substr(0,11));
+// Se especifica el inicio
+console.log(mensaje.substr(12));
+// Slice
+console.log(mensaje.slice(-30));
+console.log(mensaje.substr(-30));
+//slice y substr funcionan exactamente igual
+console.log(mensaje.split(" "));
+// replace 
+console.log(mensaje.replace("CSS", "PHP"))
+// Include
+console.log(mensaje.includes("HTML"));
+tecnologia = tecnologia + " ";
+console.log(tecnologia.repeat(10));
