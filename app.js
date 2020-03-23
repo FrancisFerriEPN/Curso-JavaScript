f0d157687de3b3cf2217227777faf40ba5fcdc78
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-22 20:43:19 -0500

    Tipos de datos y medir tiempo

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
-const numero1 = 30, numero2 = 20, numero3 = 12.123, numero4 = .0534, numero5 = -4;
+let valor ;
+valor = 'cadena';
+valor = 20;
+valor = true;
+valor = null,
+valor = undefined;
 
-let resultado;
-//Suma
-resultado = numero1 + numero2;
-// Resta
-resultado = numero1 - numero2;
-// Multiplicacion
-resultado = numero1 * numero2;
-resultado = numero1 * 2;
-// Division
-resultado = numero1 / numero2;
-// Modulo
-resultado = numero1 % numero2;
-// Pi
-resultado = Math.PI
-// Redondear
-resultado = Math.round(2.5);// Sube a 3
-resultado = Math.round(2.4);// Baja a 2
-// Techo y suelo
-resultado = Math.ceil(2.2);
-resultado = Math.floor(2.2);
-// Raiz cuadarada
-resultado = Math.sqrt(144);
-// Valor absoluto
-resultado = Math.abs(numero5);
-// Potencia
-resultado = Math.pow(8,2);
-// Minimo
-resultado = Math.min(8,6,7,6,7,8,30,9,2,5,6,7,8,9);
-// Maximo
-resultado = Math.max(8,6,7,6,7,8,30,9,2,5,6,7,8,9);
-// Aleatorio
-resultado = Math.random()*30;
-resultado = Math.round(Math.random()*30);
+valor = {
+    nombre : 'Francis',
+    apellido : "Ferri"
+}
 
-// Igual que las matematicas de secundaria
-resultado = 10 + 20 * 5;
-let puntaje = 10;
-puntaje++;
-++puntaje;
---puntaje;
-puntaje--;
-puntaje += 3;
-puntaje *= 2;
-console.log(puntaje);
+console.log(valor);
+// Calcular tiempo
+console.time("proceso");
+console.log('e esta realizando un proceso largo no te desanimes');
+console.timeEnd("proceso");
+
+// Comprobar valores
+//valor = 'cadena';
+console.log(typeof(valor));
+valor = 20;
+console.log(typeof valor);
+// Diferencia ente undefined y null
+valor = undefined;
+console.log(typeof valor);
+valor = null;
+console.log(typeof valor);
+
+// Symbol
+valor = Symbol("Simbolo");
+// Los arreglos son considerados com un objeto
+valor = [1,2,3,4];
+// Fecha es onsiderada objeto
+valor = new Date();
+console.log(typeof valor);
