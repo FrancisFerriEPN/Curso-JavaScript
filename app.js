dab23b421ca4ed360d83ade5d7d4217d1bfe1273
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-23 21:06:50 -0500

    Comparaciones numercas

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
-let valor ;
-valor = 'cadena';
-valor = 20;
-valor = true;
-valor = null,
-valor = undefined;
+const numero1 =20;
+const numero2 = 50;
+const numero3 = '20';
+console.log(numero1 > numero2);
+console.log(numero1 < numero2);
+// Usar 2 iguales comparador no estricto
+console.log(numero1 == numero3);
+// Utilizar 3 iguales comparador estricto revisa tipo de dato
+console.log(numero1 === numero3);
+console.log(typeof numero1);
+console.log(typeof numero3);
+// Diferente
+console.log(2 != 3);
+console.log("Hola" == " Hola")
 
-valor = {
-    nombre : 'Francis',
-    apellido : "Ferri"
-}
+// EL rando de letras va desde A <... z es decir que la A mayuscula es la letra de menor valor y z minuscula la de mayor
+console.log('a'<'b');
+// Z < a las inusculas sn mayores que las mayyusculas
+console.log('Z' > 'a')
+//
+console.log('z' > 'A')
 
-console.log(valor);
-// Calcular tiempo
-console.time("proceso");
-console.log('e esta realizando un proceso largo no te desanimes');
-console.timeEnd("proceso");
+// true con operador no estricto
+console.log(null == undefined);
+// false con operador estricto
+console.log(null === undefined);
 
-// Comprobar valores
-//valor = 'cadena';
-console.log(typeof(valor));
-valor = 20;
-console.log(typeof valor);
-// Diferencia ente undefined y null
-valor = undefined;
-console.log(typeof valor);
-valor = null;
-console.log(typeof valor);
-
-// Symbol
-valor = Symbol("Simbolo");
-// Los arreglos son considerados com un objeto
-valor = [1,2,3,4];
-// Fecha es onsiderada objeto
-valor = new Date();
-console.log(typeof valor);
+// UTILZA SIEMPRE EL COMPARADOR CON TRES SIGNOS DE IGUAL
