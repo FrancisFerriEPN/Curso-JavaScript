9d9d60116378f44b57a4510034e13a3eace865f8
FrancisFerriEPN <49722697+FrancisFerriEPN@users.noreply.github.com>
2020-03-23 21:30:53 -0500

    Conversion de numeross

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-const numero1 =20;
-const numero2 = 50;
-const numero3 = '20';
-console.log(numero1 > numero2);
-console.log(numero1 < numero2);
-// Usar 2 iguales comparador no estricto
-console.log(numero1 == numero3);
-// Utilizar 3 iguales comparador estricto revisa tipo de dato
-console.log(numero1 === numero3);
-console.log(typeof numero1);
-console.log(typeof numero3);
-// Diferente
-console.log(2 != 3);
-console.log("Hola" == " Hola")
+const numero1 ="50", 
+      numero2 = 10;
 
-// EL rando de letras va desde A <... z es decir que la A mayuscula es la letra de menor valor y z minuscula la de mayor
-console.log('a'<'b');
-// Z < a las inusculas sn mayores que las mayyusculas
-console.log('Z' > 'a')
-//
-console.log('z' > 'A')
+// Si se suma string con numerico concatena el valor a string
+console.log(numero1 + numero2); //"5010"
+// Es curioso pero con menos si resta por que no hay resta para strings
+console.log(numero1 - numero2); //"5010"
+// Number transforma a numero
+console.log(Number(numero1) + numero2);
+// parseInt
+console.log(parseInt(numero1) + numero2);
+console.log(typeof parseInt(numero1));
 
-// true con operador no estricto
-console.log(null == undefined);
-// false con operador estricto
-console.log(null === undefined);
+let dato;
+dato = Number("20");
+dato = Number("2.343454");
+// Devuelvve 1
+dato = Number(true);
+// Devuele 0
+dato = Number(false);
+// Devuelve 0
+dato = Number(null);
 
-// UTILZA SIEMPRE EL COMPARADOR CON TRES SIGNOS DE IGUAL
+// parseFloat y parseInt
+dato = parseInt("100");
+// ParseInt cando tiene decimales devuelve el valor como floor solo conserva la parte entera en pocas, es logico
+dato = parseInt("200.9");
+// ParseFloat cuando tiene un entero sin decimales no agrega .00 conserva el valor exacto
+dato = parseFloat("100");
+dato = parseFloat("100.234");
+console.log(dato);
+
+// toFixed
+dato =128.234567890;
+// Sin decimales
+console.log(dato.toFixed());
+// Con decimales eliges tu
+console.log(dato.toFixed(3));
+dato ="123.445678678";
+console.log(parseFloat(dato).toFixed(3));
+console.log(parseInt(dato));
