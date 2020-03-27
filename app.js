1579bb39935b2fac9af8ed4a1889e1fe8ca064f7
Cuy del diablo <francisferrir@gmail.com>
2020-03-27 17:27:35 -0500

    Objetos

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const numeros = [1,2,3,4];
-// Cnst n permitte reescribir la variable directamente
-//numeros = [7,6,3,8];
+// Crear objetos
+// Para crear un objeto se utiliza llaves
+const persona = {
+    nombre : "Miguel",
+    apellido: "Martines",
+    profesion: "diseñadr grafico",
+    email: "miguel.martinez@gmail.com",
+    edad: 20,
+    musica: ["Thrash","Rock","Grunch"],
+    // Objeto dentr de tro objeto
+    hogar : {
+        ciudad: "Quito",
+        pais: "Ecuador"
+    },
+    // Tiene la siguente manera para crear metodos
+    nacimiento : function() {
+        // Se utiliza la palabra this para referirse a los atributs del objeto
+            return new Date().getFullYear() - this.edad;
+    }
+};
+console.log(persona);
+console.log(persona.apellido);
+console.log(persona.edad);
+console.log(persona.musica[1]);
+persona.musica.push('Alternativo');
+console.log(persona.musica);
+console.log(persona.hogar.ciudad);
 
-// let si permite reescribir la variable 
-//let numeros = [1,2,3,4];
-//numeros = [7,6,3,8];
-// A pesar de ser constante se puuede reasignar valores en elarreglo
-numeros[0] = 6;
-numeros.push(5);
-numeros.unshift(9);
-console.log(numeros);
+// Existe ottra frma de acceder aos valores del objeto, n tan recomendada
+console.log(persona['hogar']["pais"]);
+// No te olvides de los parentesis
+console.log(persona.nacimiento());
