bfe9a399643215d25ced7119507d5fd05cd8437d
Cuy del diablo <francisferrir@gmail.com>
2020-03-28 21:53:21 -0500

    else if

--- app.js
+++ app.js
@@ -1,40 +1,21 @@
-const edad = 19;
-if(edad != 18){
-    console.log("Si puedes entrar");
-}else {
-    console.log("No puedes entrar");
-}
-
-
-// Comprobar que una variable tiene un valor
-
-// Si es undefined  nulo va a decir que no hay puntaje
-let puntaje;
-if(puntaje) {
-    console.log(`EL puntaje fue de ${puntaje}`);
+let hora = 19;
+// && y || 
+if(hora >0 && hora <=24){
+    if (hora  <= 10){
+        console.log(`Buens dias`);
+    }else if (hora <=18){
+        console.log(`Buenas tardes`);
+    } else {
+        console.log(`Buenas noches`);
+    }
 } else {
-    console.log(`No hay puntaje`)
-}
-//Esto permitte compprobar formularios
-console.log(puntaje);
-
-if (typeof puntaje != 'undefined'){
-    console.log(`EL puntaje es de ${puntaje}`)
-}else {
-    console.log(`No hay puntaje`);
+    console.log(`Introduzca una hora valida`);
 }
 
-let efectivo = 500;
-let total_carrito =300;
-if(efectivo >= total_carrito){
-    console.log("Pago correcto")
-}else {
-    console.log(`No dispone de fonds suficientes`)
+let puntaje = 100
+// se interrumpe cuando entra en una categoria el else if
+if(puntaje < 150) {
+    console.log('Puntaje menor a 150');
+} else if(puntaje < 200){ // Esta no se ejecuta
+    console.log(`Puntaje <200`);
 }
-
-// Tambien se puede quitar las llaves cuando es sl una instruccion la que sigue
-if(efectivo >= total_carrito)
-    console.log("Pago correcto");
-else 
-    console.log(`No dispone de fonds suficientes`);
-// Aun asi siemmpre utiliza llaves
