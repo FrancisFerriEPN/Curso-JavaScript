efb729da76a370bb3bfb86ff206194a522fd2ff5
Cuy del diablo <francisferrir@gmail.com>
2020-03-28 22:01:22 -0500

    or y and

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-let hora = 19;
-// && y || 
-if(hora >0 && hora <=24){
-    if (hora  <= 10){
-        console.log(`Buens dias`);
-    }else if (hora <=18){
-        console.log(`Buenas tardes`);
-    } else {
-        console.log(`Buenas noches`);
-    }
-} else {
-    console.log(`Introduzca una hora valida`);
+// Operador || y &&
+let efectico = 300,
+credito =300,
+totalCarrito = 500;
+disponible = efectico + credito;
+if(totalCarrito <efectico || totalCarrito < credito || totalCarrito <= disponible){
+    console.log('Puedo pagar');
+}else {
+    console.log('No puedo pagar');
 }
 
-let puntaje = 100
-// se interrumpe cuando entra en una categoria el else if
-if(puntaje < 150) {
-    console.log('Puntaje menor a 150');
-} else if(puntaje < 200){ // Esta no se ejecuta
-    console.log(`Puntaje <200`);
-}
+// Ternario
+let logueado = true;
+console.log(logueado === true ? 'Si se logueo': 'No se logueo');
