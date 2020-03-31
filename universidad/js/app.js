698f9b8a5da7a24f92495ed40547382126444ff6
Cuy del diablo <francisferrir@gmail.com>
2020-03-31 16:17:26 -0500

    Obtener elementos y cambiar propiedades

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,39 +1,17 @@
-let elemento;
-elemento = document;
-// Permite ver los elementos enlistados. Para ello debes dar clic en la flechiita e console
-elemento = document.all;
-elemento = document.all[10];
-elemento = document.head;
-elemento = document.body;
-// Este regresa la la IP
-elemento = document.domain;
-// Este nos regresa la url es decir protocolo, IP , puerto, archvo actual
-elemento = document.URL;
-// UTF-8
-elemento = document.characterSet;
-// ACceder a lo formularios
-elemento = document.forms;
-elemento = document.forms[0];
-elemento = document.forms[0].id;
-elemento = document.forms[0].className;
-// SI tuviera mas de una 
-elemento = document.forms[0].classList;
-// Array del total de imagenes
-elemento = document.images;
-elemento = document.images[2];
-// Te regresa URL completa
-elemento = document.images[2].src;
-// Te regresa carpeta y nombre de archivo
-elemento = document.images[2].getAttribute('src');
+// Get eleent by id
+//let elemento;
 
-//Cuantos scripts tiene el documento
-elemento = document.scripts;
-
-elemento = document.images;
-let imagenes = document.images;
-// Con Array. form se puede comvertir cualquier cosa iterable a arreglo
-let imagenes_array = Array.from(imagenes);
-imagenes_array.forEach(function(imagen){
-    console.log(imagen);
-});
-console.log(imagenes_array);
+//elemento = document.getElementById('header').className;
+let encabezado;
+// para obtener el texto
+encabezado = document.getElementById('encabezado').textContent;
+encabezado = document.getElementById('encabezado').innerText;
+// Para cambiar el estilo en pocas se pude modifiar el css desde JAvaScript
+encabezado = document.getElementById("encabezado");
+encabezado.style.background = "#333";
+encabezado.style.color = "#fff";
+encabezado.style.padding = '20px';
+// Cambiar texto
+encabezado.textContent = "Aprovecha el curso mi rey";
+encabezado.innerText = "Aprovecha el curso mi rey";
+console.log(encabezado.innerText);
