af94b225a88c98ce36be86e0c7530d3b82e1e723
Cuy del diablo <francisferrir@gmail.com>
2020-03-31 18:04:52 -0500

    Parent and Siblings elements 2
    Solo quite el comentario

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,11 +1,11 @@
-/*
+
 const enlaces = document.querySelectorAll(".enlace");
 console.log(enlaces[0].parentElement.parentElement.parentElement);
 // Usa element no Node
 
 const cursos = document.querySelectorAll(".card");
 console.log(cursos[0].parentElement.parentElement.parentElement.children[0].textContent = "Que mas wey");
-*/
+
 const enlaces = document.querySelectorAll(".enlace");
 // Los siblings son ls que estan al mismo nivel
 console.log(enlaces[4].previousElementSibling.previousElementSibling);
