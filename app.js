eda14ca756485629cc656e597fa129f0e3115a1d
Cuy del diablo <francisferrir@gmail.com>
2020-04-06 12:51:35 -0500

    Object create

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-function Cliente(nombre,saldo){
-    this.nombre = nombre;
-    this.saldo = saldo;
+// Object create
+const Cliente = {
+    imprimir_saldo: function(){
+        return `hola ${this.nombre}, tu saldo es: ${this.saldo}`
+    },
+    retirar_saldo: function(retiro){
+        return this.saldo -= retiro;
+    }
 }
 
-// Prototipo que imprime saldo y nombre
-Cliente.prototype.nombre_saldo_cliente = function () {
-    return `Nombre: ${this.nombre}, tu saldo es de: ${this.saldo}`;
-}
+// Crear ojeto
+const mary = Object.create(Cliente);
+// EL objeto esta vacio solo tiene metodos
+console.log(mary);
 
-const cliente1 = new Cliente("Francis",200);
-// Protoipo que imprime saldo y nombre
-// Aqui si aparece el metodo en el proto
-console.log(cliente1);
+// Le agregamos sus atributos
+mary.nombre ="Mary";
+mary.saldo = 2000;
 
-// Baca para empresas
-function Empresa(nombre, saldo, telefono, tipo){
-    // Heredar de clases
-    Cliente.call(this, nombre, saldo);
-    this.telefono = telefono;
-    this.tipo = tipo;
-}
+console.log(mary);
 
-const empresa = new Empresa('Udemy', 2000000, 12000000, "Educacion");
-// Aqui no aparece el metodo en el prototype
-console.log(empresa);
-
-// Hay que heredar el prototipo
-Empresa.prototype = Object.create(Cliente.prototype);
-
-const empresa2 = new Empresa('Coursera', 3000000, 44000000, "Educacion");
-console.log(empresa2);
-console.log(empresa2.nombre_saldo_cliente());
+console.log(mary.imprimir_saldo());
+console.log(mary.retirar_saldo(500));
