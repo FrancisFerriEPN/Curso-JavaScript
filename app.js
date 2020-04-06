6ebe60d57d887f105af230f9862b7356a2b75cef
Cuy del diablo <francisferrir@gmail.com>
2020-04-06 12:45:10 -0500

    Heredar Clases y Prototipos

--- app.js
+++ app.js
@@ -1,45 +1,33 @@
-function Cliente(nombre,salldo){
+function Cliente(nombre,saldo){
     this.nombre = nombre;
-    this.salldo = salldo;
-}
-
-// Permite guaradar  los metodos de manera segura dentro del prototipo del objeto
-// Crear un prototype
-Cliente.prototype.tipo_cliente = function(edad){
-    let tipo;
-    if(this.salldo > 1000){
-        tipo = "Gold";
-    } else if (this.salldo > 500){
-        tipo = "Platino"
-    } else {
-        tipo = "Normal";
-    }
-    return tipo;
+    this.saldo = saldo;
 }
 
 // Prototipo que imprime saldo y nombre
 Cliente.prototype.nombre_saldo_cliente = function () {
-    return `Nombre: ${this.nombre}, tu saldo es de: ${this.salldo}, Tipo cliente: ${this.tipo_cliente()} `;
-}
-
-// Retirar saldo
-Cliente.prototype.retirar_saldo = function(retiro){
-    return this.salldo -= retiro;
+    return `Nombre: ${this.nombre}, tu saldo es de: ${this.saldo}`;
 }
 
 const cliente1 = new Cliente("Francis",200);
-const cliente2 = new Cliente("Pedro",800);
-const cliente3 = new Cliente("Alfonso",1200);
+// Protoipo que imprime saldo y nombre
+// Aqui si aparece el metodo en el proto
 console.log(cliente1);
 
-console.log(cliente1.tipo_cliente());
-console.log(cliente2.tipo_cliente());
-console.log(cliente3.tipo_cliente()); 
+// Baca para empresas
+function Empresa(nombre, saldo, telefono, tipo){
+    // Heredar de clases
+    Cliente.call(this, nombre, saldo);
+    this.telefono = telefono;
+    this.tipo = tipo;
+}
 
-// Protoipo que imprime saldo y nombre
-console.log(cliente1.nombre_saldo_cliente());
-console.log(cliente2.nombre_saldo_cliente());
-console.log(cliente3.nombre_saldo_cliente());
+const empresa = new Empresa('Udemy', 2000000, 12000000, "Educacion");
+// Aqui no aparece el metodo en el prototype
+console.log(empresa);
 
+// Hay que heredar el prototipo
+Empresa.prototype = Object.create(Cliente.prototype);
 
-console.log(cliente2.retirar_saldo(300));
+const empresa2 = new Empresa('Coursera', 3000000, 44000000, "Educacion");
+console.log(empresa2);
+console.log(empresa2.nombre_saldo_cliente());
