595ccaa663ab91d3d8059947090f31de61097aaa
Cuy del diablo <francisferrir@gmail.com>
2020-04-05 22:32:02 -0500

    Objetos vs tipos de datos
    * Comparaion estricta y no estricta entre objetos y tipos de datos
    * String
    * Number
    * Boolean
    * Function
    * Object
    * Array

--- app.js
+++ app.js
@@ -1,45 +1,61 @@
-// Object literal
-const cliente = {
-    nombre : 'Francis',
-    saldo : 1200,
-    tipo_cliente : function(){
-        let tipo;
-        // Sin this (saldo) da error
-        if(this.saldo > 1000){
-            tipo = "Gold";
-        } else if (this.saldo > 500){
-            tipo = "Platinum";
-        }  else {
-            tipo = "Normal";
-        }
-        return tipo;
-    }
-}
-console.log(cliente.tipo_cliente());
-console.log(cliente.saldo);
+// String
+const nombre1 = "Pedro"; // tipo string
+const nombre2 = new String("Pedro"); // tipo objeto
 
-// Metodo alternativo
-function Cliente(nombre, saldo){
-    this.nombre = nombre;
-    this.saldo = saldo;
-    this.tipo_cliente =  function(edad){
-        let tipo;
-        // Sin this (saldo) da error
-        if(this.saldo > 1000){
-            tipo = "Gold";
-        } else if (this.saldo > 500){
-            tipo = "Platinum";
-        }  else {
-            tipo = "Normal";
-        }
-        return tipo;
-    }
-}
-// Se crean on new como en Java
-const persona = new Cliente("Francis", 300);
-const persona2 = new Cliente("Karen", 6000);
-console.log(persona);
-console.log(persona.tipo_cliente());
+console.log(typeof nombre1);
+console.log(typeof nombre2);
+console.log(nombre2);
 
+// En comparacion no estricta son considerados iguales
+if (nombre1 == nombre2){
+    console.log("Si, iguales");
+} else {
+    console.log("No, diferentes");
+}
+
+// En comparacion estricta son considerados distntos
+if (nombre1 === nombre2){
+    console.log("Si, iguales");
+} else {
+    console.log("No, diferentes");
+}
+
+// Numeros
+const numero1 = 20;
+const numero2 = new Number(20);
+console.log(numero1);
+console.log(numero2);
+
+// Booleanos
+const boolean1 = true;
+const boolean2 = new Boolean(true);
+console.log(boolean1);
+console.log(boolean2);
+
+// Funciones
+const funcion1 = function (a, b) {
+    return a + b;   
+}
+
+// Muy poco usada
+const funcion2 = new Function('a', 'b', 'return 1 + 2');
+
+console.log(funcion1(2,3));
+console.log(funcion2(1,2));
+
+
+// Objetos (Son exactamente iguales)
+const persona1 = {
+    nombre : 'Francis'
+}
+const persona2 = new Object({nombre: 'Francis'});
+
+console.log(persona1);
 console.log(persona2);
-console.log(persona2.tipo_cliente());
+
+// Arreglos (Son exactamente iguales)
+const arreglo1 = [1,2,3,4];
+const arreglo2 = new Array(1,2,3,4);
+
+console.log(arreglo1);
+console.log(arreglo2);
