dfaa337ddf099b50358800a947adb9e20f616e53
Cuy del diablo <francisferrir@gmail.com>
2020-04-06 21:46:57 -0500

    Clases y metodos estaticos

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-// Object create
-const Cliente = {
-    imprimir_saldo: function(){
-        return `hola ${this.nombre}, tu saldo es: ${this.saldo}`
-    },
-    retirar_saldo: function(retiro){
-        return this.saldo -= retiro;
+// Para ECMAScript 6
+class Cliente{
+    constructor(nombre, apellido, saldo){
+        this.nombre = nombre;
+        this. apellido = apellido;
+        this.saldo = saldo;
+    }
+
+    // Cualquier metodo que se agregue va a ser parte del protp
+    imprimir_saldo(){
+        return `Holas ${this.nombre}, tu saldo es de: ${this.saldo}`
+    }
+
+    tipo_cliente(){
+        let tipo;
+        if (this.saldo > 10000){
+            tipo = "Gold";
+        } else if (this.saldo > 5000){
+            tipo = "Platinum";
+        } else {
+            tipo = "Normal";
+        }
+        return tipo;
+    }
+
+    retirar_saldo (monto){
+        return this.saldo -= monto;
+    }
+
+    // Los metodos estaticos sirven para no tener que instanciar un objeto. Se puede acceder a los metodods desde la clase
+    static bienvenida(){
+        return `Bienvenido al cajero`;
     }
 }
 
-// Crear ojeto
-const mary = Object.create(Cliente);
-// EL objeto esta vacio solo tiene metodos
-console.log(mary);
-
-// Le agregamos sus atributos
-mary.nombre ="Mary";
-mary.saldo = 2000;
-
-console.log(mary);
-
-console.log(mary.imprimir_saldo());
-console.log(mary.retirar_saldo(500));
+const maria = new Cliente("Maria", "Peréz",11000);
+console.log(maria);
+console.log(maria.imprimir_saldo());
+console.log(maria.tipo_cliente());
+console.log(maria.retirar_saldo(30));
+console.log(Cliente.bienvenida());
