a1082e638a84aed46910fff2c4e5b3cae9c7ed8c
Cuy del diablo <francisferrir@gmail.com>
2020-04-09 23:01:56 -0500

    AJAX JSON vector

--- AJAXJSON/app.js
+++ AJAXJSON/app.js
@@ -1,5 +1,6 @@
 const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
+
 boton1.addEventListener("click", function() {
     const xhr = new XMLHttpRequest();
 
@@ -17,6 +18,36 @@ boton1.addEventListener("click", function() {
             </ul>
             `;
             document.getElementById("empleado").innerHTML= html_template;
+        }
+    }
+
+    xhr.send();
+});
+
+boton2.addEventListener("click",function() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "empleados.json", true);
+    xhr.onload = function(){
+        if(this.status === 200){
+            
+            // Directamente me hace la transformacion de arreglo
+            const personal = JSON.parse(this.responseText);
+            let html_template;
+            personal.forEach( function(persona){
+
+                // Se puede sumar
+                html_template += `
+                
+                <ul>
+                    <li>ID: ${persona.id}</li>
+                    <li>Nombre: ${persona.nombre}</li>
+                    <li>Empresa: ${persona.empresa}</li>
+                    <li>Actividades: ${persona.trabajo}</li>
+                </ul>
+                `;
+            });
+            document.getElementById("empleados").innerHTML = html_template;
         }
     }
 
