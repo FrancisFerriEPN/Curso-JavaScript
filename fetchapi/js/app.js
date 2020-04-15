07fff1b08bfb7d52d65c06cb35e6d0014e58c887
Cuy del diablo <francisferrir@gmail.com>
2020-04-14 21:23:14 -0500

    Fetch API rest

--- fetchapi/js/app.js
+++ fetchapi/js/app.js
@@ -1,5 +1,6 @@
 document.getElementById("txtBtn").addEventListener("click", cargar_txt);
 document.getElementById("jsonBtn").addEventListener("click", cargar_json);
+document.getElementById("apiBTN").addEventListener("click", cargar_rest);
 
 // Se requiere dos then el primer para conectarse y extraer la informacion
 
@@ -33,4 +34,22 @@ function cargar_json(){
     }).catch(function(error){
         console.log(error);
     })
+}
+
+function cargar_rest(){
+    fetch("https://picsum.photos/list")
+        .then(function(resp){
+            return resp.json();
+        }).then(function(imagenes){
+            let html = "";
+            imagenes.forEach(function(imagen){
+                html += `
+                    <li> 
+                        <a target="_blank" href="${imagen.post_url}">Ver imagen</a>
+                        ${imagen.autor}
+                    </li>
+                `;
+            });
+            document.getElementById('resultado').innerHTML = html;
+        })
 }
