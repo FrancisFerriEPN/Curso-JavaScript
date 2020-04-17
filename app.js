64c21b12deeaf1d5d2b70d7514e522a137e782a2
Cuy del diablo <francisferrir@gmail.com>
2020-04-16 19:35:04 -0500

    Async Await
    Funciona en ECMA Script 7

--- app.js
+++ app.js
@@ -1,69 +1,21 @@
-// Arroy function
-let aprendiendo;
-
-aprendiendo = function(){
-    console.log("Aprendiendo JavaScript");
+// Async Await
+async function obtener_clientes(){
+    const cliientes = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Clientes descargados...');
+        },2000);
+    });
+    // error o no
+    const error = true;
+    if(!error){
+        // Espera a que se cumpla el promise
+        const respuesta = await cliientes;
+        return respuesta;
+    } else {
+        await Promise.reject("Hubo un error...");
+    }
 }
 
-// Automaticamente al utilizar el  igual para la funcion la variable se volvio un metodo
-
-aprendiendo = () => {
-    console.log("Aperendiendo JavaScript");
-}
-
-
-// Cuando es una linea no se requiere llaves
-aprendiendo = () => console.log("No se requiere llaves");
-
-aprendiendo();
-
-// Retorna valor
-aprendiendo = () => "Retorno directo";
-
-
-// Objeto
-// Se utiliza parentesis como escape a las llaves
-aprendiendo = () => ({gato :"Retorno directo"});
-console.log(aprendiendo());
-
-// PAsando parametros
-aprendiendo = (tecnologia) => console.log(`Aprendiendo ${tecnologia}`);
-
-// Cuando se pasa un solo parametro el parentesis es opcional
-aprendiendo = tecnologia => console.log(`Aprendiendo ${tecnologia}`);
-
-// Si tiene dos o mas parametross los parentesis son obligatirios
-aprendiendo = (tec1, tec2) => console.log(`Aprendiendo ${tec1} y ${tec2}`);
-
-aprendiendo("JavaScript", "ES6");
-
-const productos = ["Disco", "camisa", "Guitarra"];
-
-// Se hace un callback
-
-//<<<
-
-let letras_producto = productos.map(function(producto){
-    return producto.length;
-});
-
-letras_producto = productos.map((producto) => {
-    return producto.length;
-}) 
-
-letras_producto = productos.map((producto) =>  producto.length);
-
-letras_producto = productos.map(producto =>  producto.length);
-
-console.log(letras_producto);
-
-productos.forEach(producto => {
-    console.log(producto);
-});
-
-productos.forEach(producto => console.log(producto));
-//>>>
-
-
-
-
+obtener_clientes()
+    .then(res => console.log(res))
+    .catch(error => console.log(error));
