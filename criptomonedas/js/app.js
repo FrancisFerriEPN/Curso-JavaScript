1112577e18ccd31df2f19354dc507cf7d0b6a35d
Cuy del diablo <francisferrir@gmail.com>
2020-04-20 22:02:51 -0500

    Proyecto Criptomonedas: Mensaje de error

--- criptomonedas/js/app.js
+++ criptomonedas/js/app.js
@@ -1,9 +1,12 @@
 // https://www.cryptocompare.com/cryptopian/api-keys
 // Leer el formulario
 // Variables
+const ui = new Interfaz();
+
 const formulario = document.querySelector("#formulario");
 const moneda = document.getElementById("moneda");
 const criptomoneda = document.getElementById("criptomoneda");
+
 
 // Listeners
 formulario.addEventListener("submit", (event) => {
@@ -21,6 +24,7 @@ formulario.addEventListener("submit", (event) => {
     // Comprobar que ambos campos tengas algo seleccionado
     if(moneda_seleccionada === "" || criptomoneda_seleccionada === ""){
         // Arrojar una alerta de error
+        ui.mostrar_mensaje("Ambos campos son obligatorios", "alert bg-danger text-center");
     } else {
         // Todo bien consultar la API
     }    
---
+++ criptomonedas/js/ui.js
@@ -0,0 +1,14 @@
+class Interfaz{
+    mostrar_mensaje(mensaje, clases){
+        const div = document.createElement("div");
+        div.className = clases;
+        div.appendChild(document.createTextNode(mensaje));
+        // Seleccionar mensajes
+        const div_mensajes = document.querySelector(".mensajes");
+        div_mensajes.appendChild(div);
+        console.log(div);
+        setTimeout(() => {
+            document.querySelector('.mensajes div').remove();
+        },3000);
+    }
+}
