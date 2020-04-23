4ee92240a7bbb3f335c875b1f5355ad2a2e0c9bf
Cuy del diablo <francisferrir@gmail.com>
2020-04-22 21:01:16 -0500

    Proyecto eventos: Categorias desde la API

--- eventos/js/eventbrite.js
+++ eventos/js/eventbrite.js
@@ -7,7 +7,15 @@ class EventBrite {
     }
     
     // Obtiene las categrias en init()
-    obtener_categorias(){
+    async obtener_categorias(){
         // Consultar las categorias a la rest API de EventBrite
+        const respuesta = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_personal}`);
+
+        // esperar la resuesta de las categorias y devolver un JSON
+        const categorias = await respuesta.json();
+
+        return{
+            categorias
+        }
     }
 }
--- eventos/js/interfaz.js
+++ eventos/js/interfaz.js
@@ -8,11 +8,22 @@ class Interfaz {
 
     //metodo para cuando inicialice
     init(){
-
+        this.imprimir_categorias();
     }
 
     // Imprmir categorias
     imprimir_categorias(){
-        const lista_categorias = eventBrite.obtener_categorias();
+        const lista_categorias = eventBrite.obtener_categorias()
+            .then(categorias =>{
+                const cast = categorias.categorias.categories;
+                // Seleccionar el select de categorias
+                const select_categoria = document.getElementById("listado-categorias");
+                cast.forEach(categoria => {
+                    const option = document.createElement("option");
+                    option.value = categoria.id;
+                    option.appendChild(document.createTextNode(categoria.name_localized));
+                    select_categoria.appendChild(option);
+                });
+            })
     }
 }
