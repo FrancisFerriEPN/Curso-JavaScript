63b1a943fbfdb959d1a01ce58d365a5fa3cc7d18
Cuy del diablo <francisferrir@gmail.com>
2020-04-23 20:39:39 -0500

    Proyecto eventos: Datos formulario

--- eventos/js/app.js
+++ eventos/js/app.js
@@ -1,3 +1,20 @@
 ///Instanciar ambas clases
 const eventBrite = new EventBrite();
 const ui = new Interfaz();      
+
+// Listener al buscador
+document.getElementById("buscarBtn").addEventListener("click", (e) => {
+    e.preventDefault();
+    // leer el text del input buscar
+    const texto_evento = document.getElementById("evento").value;
+    const categorias = document.getElementById("listado-categorias");
+    const categoria_seleccionada = categorias.options[categorias.selectedIndex].value;
+    // Revisar que haya algo escrito en el buscador
+    if (texto_evento === ""){
+        console.log("No hay nada");
+    } else {
+        console.log("Buscando...");
+        
+    }
+    
+});
