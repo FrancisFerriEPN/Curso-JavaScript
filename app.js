4b7ceaee6b4e6d70782287f7ad933ae1ffa47cce
Cuy del diablo <francisferrir@gmail.com>
2020-04-27 21:23:45 -0500

    Destructuring: Arreglos  Objetos
    Muy importante

--- app.js
+++ app.js
@@ -1,53 +1,56 @@
+
+const ciudades =["Londres","New York","Madrid","Paris"];
+
+const [
+    primera_ciudad,
+    segunda_ciudad
+] = ciudades; 
+
+console.log(primera_ciudad);
+console.log(segunda_ciudad);
+
+
+const [,,ciudad,paris] = ciudades;
+console.log(paris);
+console.log(ciudad);
+
+
+// cuidadoen con los elementos a lo que accedes
+const ciudades =["Londres","New York","Madrid","Paris",{idioma:"Ingles"}];
+
+console.log(ciudades);
+[idioma] = ciudades;
+console.log(idioma);
+
+
+// Esta sintaxis es mu eficaz para tratar objetos, arreglos no
+
 const cliente = {
-    nombre: "Alejandra",
-    tipo : "Premium"
+    tipo:"Premium",
+    slado: 3000,
+    datos:{
+        nombre: "Pedro",
+        apellido: "Perez",
+        residencia: {
+            ciudad: "Mexico"
+        }
+    },
+    movimientos: ['12-03-2018', '12-03-2017', '12-03-2016']
 }
 
-// Forma tipica
-let nombre = cliente.nombre,
-    tipo = cliente.tipo;
-nombre = "Pedro";
-tipo = "Gold";
 
+//<<<
 
-// nueva forma
-// Gracias a esta notacion se evita lo avalores anteriores
-({nombre, tipo} = cliente);
-console.log(nombre);
+// Tienes que especiicar el nombre de la varable
+let {
+    datos,
+    datos:{residencia},
+    tipo,
+    movimientos: [,dos]
+} = cliente;
+console.log(residencia);
 console.log(tipo);
+console.log(datos);
+console.log(dos);
 
-//
-const cliente1 = {
-    tipo:"Premium",
-    nombre:"Antonio",
-    datos:{
-        ubicacion:{
-            ciudad:"Jalisco",
-            pais: "Mexico"
-        },
-        cuenta: {
-            desde: "10-12-2012",
-            saldo: 4000
-        }
-    }
-};
-let {datos: {ubicacion}} = cliente1;
-console.log(ubicacion);
-console.log(ubicacion.ciudad);
-console.log(ubicacion.pais);
-
-
-let {datos: {cuenta}} = cliente1;
-console.log(cuenta.desde);
-console.log(cuenta.saldo);
-
-//
-const cliente3 = {
-    nombre:"Pedro",
-    tipo:"Premium",
-};
-
-({nombre, tipo="Basico", saldo=0} = cliente3);
-console.log(nombre);
-console.log(tipo);
-console.log(saldo);
+//>>>
