6696cd439255f3760c4e0fbcb7605f79c545a42a
Cuy del diablo <francisferrir@gmail.com>
2020-04-28 21:44:09 -0500

    Iteradores
    **Arrow Funtion en el return de una funcion**

--- app.js
+++ app.js
@@ -1,55 +1,25 @@
-// Maps
-let cliente = new Map();
-console.log(cliente);
+// Iteradoresç
+function crear_iterador(carrito){
+    let i = 0;
+    return {
+        siguiente: () =>{
+            let fin = (i >= carrito.length);
 
-cliente.set('nombre','Francis');
-cliente.set('tipo','Premium');
-cliente.set('saldo',3000);
-console.log(cliente);
+            let valor = !fin ? carrito[i++] : undefined;
 
-// Aceeder a los valores
-console.log(cliente.get("nombre"));
-console.log(cliente.get("tipo"));
-console.log(cliente.get("saldo"));
+            return{
+                fin:fin,
+                valor:valor
+            }
+        }
+    }
+}
 
-// Metodos para maps
-console.log(cliente.size);
-console.log(cliente.has("tipo"));
-console.log(cliente.has("apellido"));
+const carrito =["Producto1","Producto2","Producto3","Producto4"];
 
-// Borrar un elemento del MAP
-cliente.delete("nombre");
-console.log(cliente.has("nombre"));
-console.log(cliente.size);
-
-// Limpiar el MAP
-cliente.clear();
-console.log(cliente);
-
-// Poner valores predetyerminados en el MAP
-const paciente = new Map(
-    [["nombre", "paciente"],
-    ["habitacion", "No definido"]
-    ]
-);
-
-console.log(paciente);
-paciente.set("nombre","Antonio");
-console.log(paciente);
-paciente.set("habitacion",400);
-paciente.set("habitacion",500);
-console.log(paciente);
-
-// Recorrer con forEach
-// Obtiene los valores directamente
-paciente.forEach(datos => {
-    console.log(datos);
-    
-});
-
-// Obtiene tambien el nombre de la llave 
-// Obtiene los valores directamente
-paciente.forEach((dato, index) => {
-    console.log(`Llave:${index} - Dato:${dato}`);
-    
-});
+const recorrer_carrito = crear_iterador(carrito);
+console.log(recorrer_carrito.siguiente());
+console.log(recorrer_carrito.siguiente());
+console.log(recorrer_carrito.siguiente());
+console.log(recorrer_carrito.siguiente());
+console.log(recorrer_carrito.siguiente());
