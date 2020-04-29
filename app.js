f5d5ea8b1fce55a22704774a38a5cf97548082a4
Cuy del diablo <francisferrir@gmail.com>
2020-04-28 21:06:15 -0500

    Sets
    * El set no permite elementos duplicados
    * Comprobar si un valor existe
    * Convertir un set a un arreglo
    Y otros metodos

--- app.js
+++ app.js
@@ -1,38 +1,50 @@
-// Symbol
-
-// No se necesita la palabra new por qu son primitivos de JavaScript
-const simbolo = Symbol();
-const simbolo2 = Symbol("Descripccion aqui");
-
-// Los simbolos son unicos en la ejecucion de JavaScipt 
-console.log(Symbol() == Symbol());
+let carrito = new Set();
 
 
-console.log(typeof simbolo);
-console.log(simbolo);
-console.log(simbolo2);
+carrito.add("Camisa");
+carrito.add("Disco #1");
+carrito.add("Disco #2");
+carrito.add("Disco #3");
 
-let nombre = Symbol();
-let apellido = Symbol();
+// <<<
+// El set no permite elementos duplicados
+carrito.add("Disco #2");
+//>>>
 
-let persona = {};
-persona.nombre = "Francis";
-// Un Symbol se agrega de la siguiente manera
-persona[nombre] = "Juan";
-persona[apellido] = "De la torre";
-persona.saldo = 100;
-persona.tipo_cliento = "Normal";
+console.log(carrito);
+console.log(carrito.size);
 
-console.log(persona);
+let numeros = new Set([1,2,3,4,5,6,7,1,2,3,4]);
+console.log(numeros);
+console.log(numeros.size);
 
+//<<<
 
-// Se accede all valor del Symbol de la siguiente manera
-console.log(persona.nombre);
-console.log(persona[nombre]);
+// comprobar si un valor existe
+console.log(carrito.has("Camisa"));
+console.log(carrito.has("Guitarra"));
 
+// borrar elemento
+carrito.delete("Camisa");
+console.log(carrito);
 
-// Los Symbol no pueden ser iterados n se puede acceder con for
-console.log("\nSolamente imprime los valores del objeto no Symbol");
-for (let i in persona){
-    console.log(`${i}: ${persona[i]}`);
-}
+// Iterar
+carrito.forEach(producto => {
+    console.log(producto);
+});
+
+// No tienen llave por eso devuelven cosas como
+// Producto: Disco #1 Posicion:Disco #1
+// En el set la llave y el valo son iguales
+carrito.forEach((producto,index) => {
+    console.log(`Producto: ${producto} Posicion:${index}`);
+});
+
+// Convertir un set a un arreglo
+const arreglo_carrito  = [...carrito];
+console.log(arreglo_carrito);
+
+// limpiar Set
+carrito.clear();
+console.log(carrito);
+//>>>
