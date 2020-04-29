ecaf6eff0558179ddb7af3379eaff8d073746dcd
Cuy del diablo <francisferrir@gmail.com>
2020-04-28 22:33:31 -0500

    Expresiones regulares parte 1

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-// Generadores
-// Est es ECMASCript 6
-function *crear_generador(){
-    // yield
-    yield 1;
-    yield "Nombre";
-    yield 3+3;
-    yield true;
-}
 
-const iterador = crear_generador();
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
-console.log(iterador.next().value);
+// Existen estas dos formas
+const exp1 = new RegExp('/abc/');
+const exp2 = /abc/;
+console.log(exp1);
+console.log(exp2);
 
-function *nuevo_generador(carrito){
-    for(let i=0; i < carrito.length; i++){
-        yield carrito[i];
-    }
-}
-// creamos el carrito carrito
-const carrito = ['Producto1','Producto2','Producto3','Producto4'];
+let valor, exp_Reg;
+exp_Reg = /[0-9]/;
+valor = 1992;
+// Probar expresion regular
+console.log(exp_Reg.test(valor));
+valor = "1992";
+console.log(exp_Reg.test(valor));
 
-// recorremos el iterador
-let nuevo_iterador = nuevo_generador(carrito);
+valor = "Hola";
+console.log(exp_Reg.test(valor));
 
-console.log(nuevo_iterador.next().value);
-console.log(nuevo_iterador.next().value);
-console.log(nuevo_iterador.next().value);
-console.log(nuevo_iterador.next().value);
-console.log(nuevo_iterador.next().value);
-console.log(nuevo_iterador.next().done);
+// Validar una fecha ccon expresion regular 20-10-2020
+valor = "20-10-2020";
+exp_Reg = /\d\d-\d\d-\d\d\d\d/;
+console.log(exp_Reg.test(valor));
+valor = "Hola mundo";
+console.log(exp_Reg.test(valor));
+
+// Validar una hora 10:30
+valor = "10:30";
+exp_Reg = /\d\d:\d\d \D\D/;
+console.log(exp_Reg.test(valor));
+valor = "10:30 AM";
+console.log(exp_Reg.test(valor));
+
+valor = "10:30 PM";
+console.log(exp_Reg.test(valor));
+
+valor = "10:30 30";
+console.log(exp_Reg.test(valor));
+
+exp_Reg = /\d\d\d\d/;
+valor = "hola mundo";
+console.log(exp_Reg.test(valor));
+valor = "hola";
+console.log(exp_Reg.test(valor));
+valor = "1234";
+console.log(exp_Reg.test(valor));
+exp_Reg = /\d+/;
+console.log(exp_Reg.test(valor));
