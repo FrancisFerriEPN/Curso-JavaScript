06eafa7d939c185a872aac7a7bd62b644d69d5d9
Cuy del diablo <francisferrir@gmail.com>
2020-04-30 21:54:01 -0500

    Modulos extender clase

--- moduloes-es6/js/app.js
+++ moduloes-es6/js/app.js
@@ -2,7 +2,7 @@ import {nombre_cliente, ahorro, mostrar_informacion, Cliente} from './cliente.js
 
 
 //<<<
-import { nombre_empresa, ahorro as ahorro_empresa, categoria, mostrar_informacion as mostrar_informacion_empresa} from './empresa.js';
+import { nombre_empresa, ahorro as ahorro_empresa, categoria, mostrar_informacion as mostrar_informacion_empresa, Empresa} from './empresa.js';
 //>>>
 
 console.log(nombre_cliente);
@@ -12,7 +12,11 @@ console.log(ahorro);
 console.log(ahorro_empresa);
 console.log(categoria);
 
+let empresa = new Empresa(nombre_empresa,ahorro_empresa,categoria);
+console.log(empresa.mostrar_informacion());
+
 const info = mostrar_informacion(nombre_cliente, ahorro);
 console.log(info);
 const info_empresa = mostrar_informacion_empresa(nombre_empresa, ahorro, categoria);
 console.log(info_empresa);
+
--- moduloes-es6/js/empresa.js
+++ moduloes-es6/js/empresa.js
@@ -1,3 +1,5 @@
+import {Cliente} from './cliente.js';
+// Exportar variables
 export const nombre_empresa = "Udemy";
 export let ahorro = 20000000;
 export const categoria = "Aprendizaje";
@@ -5,4 +7,16 @@ export const categoria = "Aprendizaje";
 // Exportar funciones
 export function mostrar_informacion(nombre, ahorro, categoria){
     return `Cliente: ${nombre} - Ahorro ${ahorro} - Categoria: ${categoria}`;
+}
+
+// Utilizar la clase del modulo clientes
+export class Empresa extends Cliente{
+    constructor(nombre, ahorro){
+        super(nombre, ahorro);
+        this.categoria = categoria;
+    }
+
+    mostrar_informacion(){
+        return `Cliente: ${this.nombre} - Ahorro ${this.ahorro} - Categoria: ${this.categoria}`;
+    }
 }
