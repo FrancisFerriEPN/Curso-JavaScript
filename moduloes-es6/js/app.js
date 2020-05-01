352128885af5872bdbc991ab145995ae081f6b11
Cuy del diablo <francisferrir@gmail.com>
2020-04-30 21:08:01 -0500

    Modulos creacion

---
+++ moduloes-es6/js/app.js
@@ -0,0 +1,12 @@
+import {nombre_cliente, ahorro} from './cliente.js';
+
+import * as cliente from './cliente.js';
+console.log(nombre_cliente);
+console.log(ahorro);
+
+console.log(cliente);
+console.log(cliente.nombre_cliente);
+console.log(cliente.ahorro);
+/* console.log(ahorro);
+const info = mostrar_informacion(nombe_cliente, ahorro);
+console.log(info); */
---
+++ moduloes-es6/js/cliente.js
@@ -0,0 +1,6 @@
+export const nombre_cliente = "Juan";
+export let ahorro = 200;
+
+function mostrar_informacion(nombre, ahorro){
+    return `Cliente: ${nombre} - Ahorro ${ahorro}`;
+}
