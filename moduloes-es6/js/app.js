3699d4dbfb5d1638edfc2d4a6ad0a0f4f38232d7
Cuy del diablo <francisferrir@gmail.com>
2020-04-30 21:27:22 -0500

    Modulos importar exportar clases

--- moduloes-es6/js/app.js
+++ moduloes-es6/js/app.js
@@ -1,11 +1,17 @@
-import {nombre_cliente, ahorro, mostrar_informacion, mostar_nombre} from './cliente.js';
-
+import {nombre_cliente, ahorro, mostrar_informacion, Cliente} from './cliente.js';
+// Usa el comodin solo para cuando vas a usar todo de lo contrario haces lento el programa
 import * as cliente from './cliente.js';
 console.log(nombre_cliente);
 console.log(ahorro);
-console.log(mostar_nombre(nombre_cliente));
 console.log(cliente);
 console.log(cliente.nombre_cliente);
 console.log(cliente.ahorro);
 const info = mostrar_informacion(nombre_cliente, ahorro);
 console.log(info);
+
+// Utiolizar la clase
+let nuevo_cliente = new Cliente(nombre_cliente,ahorro);
+console.log(nuevo_cliente);
+console.log(nuevo_cliente.mostrar_informacion());
+let nuevo_cliente2 = new cliente.Cliente(cliente.nombre_cliente,cliente.ahorro);
+console.log(nuevo_cliente2);
--- moduloes-es6/js/cliente.js
+++ moduloes-es6/js/cliente.js
@@ -1,10 +1,18 @@
 export const nombre_cliente = "Juan";
 export let ahorro = 200;
 
+// Exportar funciones
 export function mostrar_informacion(nombre, ahorro){
     return `Cliente: ${nombre} - Ahorro ${ahorro}`;
 }
 
-export function mostar_nombre(nombre){
-    return `Nombre del cliente: ${nombre}`;
+// Exportar u7na clase
+export class Cliente{
+    constructor(nombre, ahorro){
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+    mostrar_informacion(){
+    return `Cliente: ${this.nombre} - Ahorro ${this.ahorro}`;
+    }
 }
