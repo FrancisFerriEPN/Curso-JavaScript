a2345f07d1b42b356fed7d61e697894e19ed7be7
Cuy del diablo <francisferrir@gmail.com>
2020-04-30 21:16:04 -0500

    Modulos exportar funciones

--- moduloes-es6/js/app.js
+++ moduloes-es6/js/app.js
@@ -1,12 +1,11 @@
-import {nombre_cliente, ahorro} from './cliente.js';
+import {nombre_cliente, ahorro, mostrar_informacion, mostar_nombre} from './cliente.js';
 
 import * as cliente from './cliente.js';
 console.log(nombre_cliente);
 console.log(ahorro);
-
+console.log(mostar_nombre(nombre_cliente));
 console.log(cliente);
 console.log(cliente.nombre_cliente);
 console.log(cliente.ahorro);
-/* console.log(ahorro);
-const info = mostrar_informacion(nombe_cliente, ahorro);
-console.log(info); */
+const info = mostrar_informacion(nombre_cliente, ahorro);
+console.log(info);
--- moduloes-es6/js/cliente.js
+++ moduloes-es6/js/cliente.js
@@ -1,6 +1,10 @@
 export const nombre_cliente = "Juan";
 export let ahorro = 200;
 
-function mostrar_informacion(nombre, ahorro){
+export function mostrar_informacion(nombre, ahorro){
     return `Cliente: ${nombre} - Ahorro ${ahorro}`;
+}
+
+export function mostar_nombre(nombre){
+    return `Nombre del cliente: ${nombre}`;
 }
