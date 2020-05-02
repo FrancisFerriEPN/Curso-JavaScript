8aa7a5e543f88e1932f023415c0694ba758cbc40
Cuy del diablo <francisferrir@gmail.com>
2020-05-01 20:40:22 -0500

    Proyecto Canciones: Interfaz
    ```javascript
    import * as UI from "./interfaz.js"
    console.log(UI);
    ```

---
+++ letrasjs-inicio/js/interfaz.js
@@ -0,0 +1,4 @@
+export const formulario_buscar = document.querySelector("#formulario-buscar"),
+    div_buscar = document.querySelector("#buscar"),
+    div_mensajes = document.querySelector("#mensajes"),
+    div_resultado = document.querySelector("#resultado");
