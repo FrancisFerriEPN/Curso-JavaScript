014932d11f890d83c2ae8df39b1d50c4c2757410
Cuy del diablo <francisferrir@gmail.com>
2020-05-01 20:53:54 -0500

    Proyecto Canciones: Validar formulario

--- letrasjs-inicio/js/app.js
+++ letrasjs-inicio/js/app.js
@@ -1,2 +1,32 @@
 import * as UI from "./interfaz.js"
-console.log(UI);
+UI.formulario_buscar.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Exporta lo que es estatico por que si exportas text bioxes o asi se mandan como estaban inicialemnte
+
+    // Obtener datos del formulraio
+    const artista = document.querySelector("#artista").value,
+        cancion = document.querySelector("#cancion").value;
+
+    if (!(artista && cancion)){
+        // El usuario deja los campos vacios mostrar error
+        UI.div_mensajes.innerHTML = 'Error... todos los campos son obligatorios';
+        UI.div_mensajes.classList.add('error');
+        setTimeout(() => {
+            UI.div_mensajes.innerHTML = '';
+        UI.div_mensajes.classList.remove('error');
+        },3000);
+    } else {
+        // El formulario esta completo, realizar consulta a la API
+        console.log(artista);
+        console.log(cancion);
+    }
+
+    /* 
+    if (artista === "" || cancion === ""){
+
+    } else {
+
+    } */
+
+})
