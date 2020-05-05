1ba441e518d0008d169e19c5c226e084c655fd2c
Cuy del diablo <francisferrir@gmail.com>
2020-05-04 22:04:17 -0500

    Design Patterns: Patron Factory parte 2
    Importante este patron de diseño ayuda mucho
    
    ```javascript
    elemento.setAttribute('placeholder', this.texto);
    ```

--- patrones/js/factory.js
+++ patrones/js/factory.js
@@ -10,11 +10,37 @@ function Constructor_sitios(){
         } else if (tipo === "p"){
             html = new ParrafoHTML(texto);
         }
+        html.tipo = tipo;
+        html.mostrar = function(){
+            const elemento = document.createElement(this.tipo);
+            if(this.tipo === "input"){
+                
+                //<<<
+                // Esta funcion no sale por defecto
+                elemento.setAttribute('placeholder', this.texto);
+                //>>>
+                
+            }else if(this.tipo === "img"){
+                elemento.setAttribute('src', this.texto);              
+            }else {
+                elemento.appendChild(document.createTextNode(this.texto));               
+            }
+            document.querySelector("#app").appendChild(elemento);
+        }
         return html;
     }
 }
 
+const InputHTML = function (texto) {
+    this.texto = texto;
+}
+const ImagenHTML = function (texto) {
+    this.texto = texto;
+}
 const HeadingHTML = function (texto) {
+    this.texto = texto;
+}
+const ParrafoHTML = function (texto) {
     this.texto = texto;
 }
 
@@ -23,4 +49,14 @@ const sitio_web = new Constructor_sitios();
 // almacenar los elementos
 const elemetos_web = [];
 elemetos_web.push(sitio_web.crear_elemmento('bienvenidos', 'h1'));
-console.log(sitio_web);
+elemetos_web.push(sitio_web.crear_elemmento('bienvenidos a mi nuevo sitio web', 'p'));
+elemetos_web.push(sitio_web.crear_elemmento('logo.svg', 'img'));
+elemetos_web.push(sitio_web.crear_elemmento('Conoce mas sobre nosotros', 'h1'));
+elemetos_web.push(sitio_web.crear_elemmento('Contacto', 'input'));
+elemetos_web.push(sitio_web.crear_elemmento('Contactanos en el formulario', 'h1'));
+
+
+console.log(elemetos_web);
+elemetos_web.forEach(elemento => {
+    elemento.mostrar();
+})
