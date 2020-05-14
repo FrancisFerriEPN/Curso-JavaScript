eb439fb75f61961c5018111d781dddb81c265b7a
Cuy del diablo <francisferrir@gmail.com>
2020-05-13 21:05:20 -0500

    Proyecto IndexedDB: Variables

---
+++ indexedDB/js/app.js
@@ -0,0 +1,11 @@
+let DB;
+// Selectores de la interfaz
+const form = document.querySelector("form"),
+    nombre_mascota = document.querySelector("#mascota"),
+    nombre_cliente = querySelector("#cliente"),
+    telefono = document.querySelector("#telefono"),
+    fecha = document.querySelector("#fecha"),
+    hora = document.querySelector("#hora"),
+    sintomas = document.querySelector("#sintomas"),
+    citas = document.querySelector("#citas"),
+    heading_administra = document.querySelector("#administra");
