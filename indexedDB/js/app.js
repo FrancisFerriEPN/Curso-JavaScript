b3ad373f6ea9249206a5cb24ca06548ab38ea80e
Cuy del diablo <francisferrir@gmail.com>
2020-05-14 22:03:00 -0500

    Proyecto IndexedDB: Creando base de datos

--- indexedDB/js/app.js
+++ indexedDB/js/app.js
@@ -2,10 +2,32 @@ let DB;
 // Selectores de la interfaz
 const form = document.querySelector("form"),
     nombre_mascota = document.querySelector("#mascota"),
-    nombre_cliente = querySelector("#cliente"),
+    nombre_cliente = document.querySelector("#cliente"),
     telefono = document.querySelector("#telefono"),
     fecha = document.querySelector("#fecha"),
     hora = document.querySelector("#hora"),
     sintomas = document.querySelector("#sintomas"),
     citas = document.querySelector("#citas"),
     heading_administra = document.querySelector("#administra");
+
+    // Esperar or el DOM ready
+    document.addEventListener("DOMContentLoaded", () => {
+
+        //<<<
+        let crear_DB = window.indexedDB.open("citas", 1);
+
+        // Si hay un errror enviasrlo a la consoiola
+        crear_DB.onerror =function() {
+            console.log("Hubo un error");
+        }
+
+        // Si todo eta bine mostratr en consola
+        crear_DB.onsuccess = function () {
+            console.log("Todo listo!");
+            // Asignar a la base de datos
+            DB = crear_DB.result;
+            console.log(DB);
+        }
+        //>>>
+        
+    });
