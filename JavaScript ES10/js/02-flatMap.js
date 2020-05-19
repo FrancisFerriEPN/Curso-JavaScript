1527e303bf6183a8dbbb5a5386099ae9bb06f81d
Cuy del diablo <francisferrir@gmail.com>
2020-05-18 21:46:19 -0500

    ES 10: flatMap

--- JavaScript ES10/js/02-flatMap.js
+++ JavaScript ES10/js/02-flatMap.js
@@ -3,3 +3,10 @@ const productos = [
     { nombre: 'Producto 2', precio: 30},
     { nombre: 'Producto 3', precio: 40}
 ];
+
+const resultado = productos.map(producto => [producto.nombre, producto.precio]);
+console.log(resultado.flat(1));
+
+// Flatmap no carga directamente
+const resultado2 = productos.flatMap(producto => [producto.nombre, producto.precio]);
+console.log(resultado2);
