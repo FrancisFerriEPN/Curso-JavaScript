f364d9720e7077ddfdf4e0b496ff50e9f15ba12c
Cuy del diablo <francisferrir@gmail.com>
2020-05-19 20:31:47 -0500

    Implicit biding

---
+++ JavaScript Avanzado/js/01.js
@@ -0,0 +1,23 @@
+function persona(nombre) {
+    console.log(`Hola soy ${nombre}`);
+}
+persona("Juan");
+
+// This con implicit binding
+const usuario = {
+    nombre: 'Juan',
+    edad: 20,
+    presentacion() {
+        console.log(`Mi nombre es ${this.nombre} y tengo ${this.edad}`);
+    },
+    mascota: {
+        nombre: "Hook",
+        edad: 1,
+        presentacion(){
+            console.log(`Mi nombre es ${this.nombre} y tengo ${this.edad}`);  
+        }
+    }
+}
+
+usuario.presentacion();
+usuario.mascota.presentacion();
