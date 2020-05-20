4aad8a71ab5b474113dca59c264b9127dc8af648
Cuy del diablo <francisferrir@gmail.com>
2020-05-19 22:18:42 -0500

    Composicion parte 2

--- JavaScript Avanzado/js/08.js
+++ JavaScript Avanzado/js/08.js
@@ -4,6 +4,32 @@ const obtener_nombre = (info) =>({
         console.log(`Nombre: ${info.nombre}`);
     }
 });
+
+const guardar_email = (info) => ({
+    email(email){
+        console.log(`Guardando email en ${info.nombre}`);
+        info.email = email;
+    }
+});
+
+const obtener_email = (info) =>({
+    mostrar_email(){
+        console.log(`Correo: ${info.email}`);
+    }
+});
+
+const obtener_empresa = (info) =>({
+    mostrar_empresa(){
+        console.log(`Empresa: ${info.empresa}`);
+    }
+});
+
+const obtener_puesto = (info) =>({
+    mostrar_puesto(){
+        console.log(`Puesto: ${info.puesto}`);
+    }
+});
+
 function Cliente(nombre, email, empresa) {
     let info = {
         nombre,
@@ -13,7 +39,10 @@ function Cliente(nombre, email, empresa) {
 
     return Object.assign(
         info,
-        obtener_nombre(info)
+        obtener_nombre(info),
+        guardar_email(info),
+        obtener_email(info),
+        obtener_empresa(info)
     );
 }
 function Empleado(nombre, email, puesto) {
@@ -24,10 +53,20 @@ function Empleado(nombre, email, puesto) {
     }
     return Object.assign(
         info,
-        obtener_nombre(info)
+        obtener_nombre(info),
+        guardar_email(info),
+        obtener_email(info),
+        obtener_puesto(info)
     );
 }
-const cliente = Cliente("Juan");
+const cliente = Cliente("Juan", null, "Udemy");
 cliente.mostrar_nombre();
-const empleado = Empleado("Pedro");
+cliente.email("cliente@cliente.com");
+cliente.mostrar_email();
+cliente.mostrar_empresa();
+
+const empleado = Empleado("Pedro", null, "Programador");
 empleado.mostrar_nombre();
+empleado.email("empleado@empleado.com");
+empleado.mostrar_email();
+empleado.mostrar_puesto();
