975000bc978dee6d4962f7f230480d4832cc95e9
Francis Ferri <francisferrir@gmail.com>
2020-06-24 20:20:37 -0500

    Node Pagina Web: Variables de entorno con dotenv

--- introNodeJS/server/config/database.js
+++ introNodeJS/server/config/database.js
@@ -1,8 +1,11 @@
 const Sequelize = require("sequelize");
+require("dotenv").config({path: "variables.env"})
 
-module.exports = new Sequelize("agenciadeviajes", "root","", {
-    host:'127.0.0.1',
-    port: '3306',
+console.log(process.env.BD_HOST)
+module.exports = new Sequelize(process.env.BD_NOMBRE, 
+    process.env.BD_USER, process.env.BD_PASS, {
+    host: process.env.BD_HOST,
+    port: process.env.BD_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
