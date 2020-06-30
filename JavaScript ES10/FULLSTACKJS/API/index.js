03e67829d5a20b4a3da25a862204be205fcc2b25
Francis Ferri <francisferrir@gmail.com>
2020-06-29 21:06:28 -0500

    FullStack JavaScript: Conectando a Mongo
    Se debe instalar mongo, este ya viene con el DBMS en el mismo paquete

--- JavaScript ES10/FULLSTACKJS/API/index.js
+++ JavaScript ES10/FULLSTACKJS/API/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
 // Crear el servidor
 const app = express();
+
+// Conectar a mongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/veterinaria",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModift: false
+})
 
 // puerto y arrancar el servidor
 app.listen(4000, () =>{
