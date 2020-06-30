c455e0313d85236e9694f508292540a16b5f0cc5
Francis Ferri <francisferrir@gmail.com>
2020-06-29 21:29:39 -0500

    FullStack JavaScript: Controlador

---
+++ JavaScript ES10/FULLSTACKJS/API/controllers/pacienteControllers.js
@@ -0,0 +1,8 @@
+
+
+// Cuando se crea un nuevo cliente
+exports.nuevoCLientte = (req, res, next) => {
+    // TODO : Insertar en la base de ddatos
+    res.json({mensaje: "El cliente se agrego correctamente"});
+
+}
