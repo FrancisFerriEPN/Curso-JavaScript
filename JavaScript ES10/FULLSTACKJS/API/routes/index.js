ffc43772ce393dca30638459a5b308d90e71e307
Francis Ferri <francisferrir@gmail.com>
2020-06-30 20:40:47 -0500

    FullStack JavaScript API: Peticiones servidor

--- JavaScript ES10/FULLSTACKJS/API/routes/index.js
+++ JavaScript ES10/FULLSTACKJS/API/routes/index.js
@@ -5,6 +5,10 @@ const pacienteController = require("../controllers/pacienteControllers")
 module.exports = function(){
     
     // Agrega nuevos pacientes via post
+    
+    router.post("/",() => {
+        console.log("En Home");
+    })
     router.post("/pacientes",
         pacienteController.nuevoCLientte
     )
