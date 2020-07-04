eacf5c727589db0267ae9ce127f09d3074faa023
Francis Ferri <francisferrir@gmail.com>
2020-07-03 21:00:58 -0500

    FullStack JavaScript APP: COnsultando API con Axios

--- JavaScript ES10/FULLSTACKJS/frontend/src/App.js
+++ JavaScript ES10/FULLSTACKJS/frontend/src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import clienteAxios from './config/axios'
 // Componentes import
 import Pacientes from './components/Pacientes';
 import NuevaCita from './components/NuevaCita';
 import Cita from './components/Cita';
 
 function App() {
+
+
+  // State de la app
+  const [citas, guardarCitas] = useState([]);
+  useEffect(
+    () => {
+      const consultarAPI = () => {
+        clienteAxios.get('/pacientes')
+        .then(respuesta => {
+          console.log(respuesta);
+        })
+        .catch (error => {
+          console.log(error);
+        })
+      }
+      consultarAPI();
+    }, []);
 
   return (
     <Router>
