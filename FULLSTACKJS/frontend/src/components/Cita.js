c8aac1ca16fbc6eeda5d28e16647bed7f7c374d2
Francis Ferri <francisferrir@gmail.com>
2020-07-08 20:56:06 -0500

    FullStack JavaScript APP: Eliminar cita

--- FULLSTACKJS/frontend/src/components/Cita.js
+++ FULLSTACKJS/frontend/src/components/Cita.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
-import {Link, withRouter} from "react-router-dom"
+import {Link, withRouter} from "react-router-dom";
+import clienteAxios from "../config/axios"
 
 const Cita = (props, {cita}) => {
 
@@ -9,7 +10,19 @@ const Cita = (props, {cita}) => {
     }
 
     // extraer por props
-    const {cita:{nombre, propietario, fecha, hora, telefono, sintomas}} = props;
+    const {cita:{_id, nombre, propietario, fecha, hora, telefono, sintomas}} = props;
+
+    // elimina un registro
+    const eliminarCita = id => {
+        clienteAxios.delete(`/pacientes/${id}`)
+            .then(respuesta => {
+                props.guardarConsultar(true);
+                props.history.push("/");
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
 
     return (
         <Fragment>
@@ -39,7 +52,8 @@ const Cita = (props, {cita}) => {
                                 </div>
                                 <div className="d-flex">
                                     <button type="button"
-                                    className="text-uppercase py-2 px-5 font-weight-bold btn btn-danger col">
+                                    className="text-uppercase py-2 px-5 font-weight-bold btn btn-danger col"
+                                    onClick={ () => eliminarCita(_id) }>
                                         Eliminar &times;
                                     </button>
                                 </div>
